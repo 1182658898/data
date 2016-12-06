0a259864122aa470a9c40936b74745ea28c54ca7
1182658898 <1182658898@qq.com>
2016-12-06 17:40:17 +0800

    4

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a = 3;
+            Double b = 1.3;
+            Console.WriteLine(a/b);
+        
+            Console.Read();
+        }
+    }
+}
